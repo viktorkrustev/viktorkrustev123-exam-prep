73c4d9a2ab2a7f0a1c9b395418d1db8948b91248
viktorkrustev <viktorkrustev03@abv.bg>
2024-10-08 14:29:46 +0300

    Fix failing tests

--- tests/ui/home.test.js
+++ tests/ui/home.test.js
@@ -4,6 +4,6 @@ test('Check home page', async ({ page }) => {
     await page.goto('http://localhost:8080/');
     const heading = await page.$('p');
     const text = await heading.textContent();
-    expect(text).toContain('My grades list');  
+    expect(text).toContain('Grades: ');  
   });
   
